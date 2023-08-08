64b33613b7414d45c60d908980de94cc1e70f018
Faruks23 <mdfaruksp2@gmail.com>
2023-08-08 14:07:41 +0600

    Creat Home page and Setup Router

--- src/Component/Home/Home.tsx
+++ src/Component/Home/Home.tsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>This is home</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis atque nostrum id sed, enim laboriosam illo odio ea ipsam fugit esse accusamus accusantium quod culpa quo unde alias! Error, eligendi!</p>
     </div>
   );
 };
