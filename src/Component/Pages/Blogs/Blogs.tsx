2a0ac1803c04dc7fc588c3327d10b0c70c63fd3d
Azharul Islam <azharulip@gmail.com>
2023-08-09 18:23:54 +0600

    blog detals done

--- src/Component/Pages/Blogs/Blogs.tsx
+++ src/Component/Pages/Blogs/Blogs.tsx
@@ -1,5 +1,6 @@
 import  { useEffect, useState } from 'react';
 import Spinner from '../Spinner/Spinner';
+import { Link } from 'react-router-dom';
 
 const Blogs = () => {
     const [blogs,setBloags]=useState([])
@@ -34,9 +35,9 @@ const Blogs = () => {
     return (
         <div>
             <section className='title flex justify-center items-center gap-5'>
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col items-center justify-start">
                   <p className='text-3xl font-bold'>From</p>
-                  <p className='text-3xl font-bold'>The</p>
+                  <p className='text-3xl font-bold text-left'>The</p>
                 </div>
                 <p className='text-8xl font-bold'>Blogs</p>
             </section>
@@ -44,11 +45,16 @@ const Blogs = () => {
                 <div className="w-[96%] md:w-[90%] mx-auto">
                   {
                     blogs.map((blog)=>(
-                      <div className="grid grid-cols-3 gap-2 items-center py-3">
-                        <p className='text-2xl'>{blog.blog_name}</p>
-                        <img src={blog.image} alt="" className='w-full h-[300px]'/>
-                        <p className='font-bold'>{blog.blog_short_description}</p>
-                        
+                      <div className="">
+                        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 md:gap-4  py-3">
+                        <p className='text-2xl text-justify'>{blog.blog_name}</p>
+                        <img src={blog.image} alt="" className='w-full h-[300px] mt-5'/>
+                        <p className='font-bold text-justify mt-5'>{blog.blog_short_description}</p>
+
+                      </div>
+                      <div className="flex justify-center">
+                      <Link className='text-center py-1 px-3 bg-transparent border border-fuchsia-900 font-bold hover:bg-black hover:text-white' to={`blog/${blog._id}`}>Details</Link>
+                      </div>
                       </div>
                     ))
                   }
