8088a5ed20062ba882f58a36c13fbc4c93e21b2e
Rashik <smarashik00@gmail.com>
2023-08-09 17:32:47 +0600

    review section added

--- src/Component/Home/Home.tsx
+++ src/Component/Home/Home.tsx
@@ -3,12 +3,14 @@ import Banner from "../Banner/Banner";
 import HeroSection from "../HeroSection/HeroSection";
 import Process from "../Process/Process";
 import React from "react";
+import Review from "../Review/Review";
 const Home = () => {
   return (
     <div>
       <Banner />
       <HeroSection />
       <Process />
+      <Review />
       <Blogs></Blogs>
     </div>
   );
