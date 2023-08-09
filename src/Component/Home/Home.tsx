988edd985e2be5225d82862564e4dd837478b869
Rashik <smarashik00@gmail.com>
2023-08-09 14:48:26 +0600

    customized home page

--- src/Component/Home/Home.tsx
+++ src/Component/Home/Home.tsx
@@ -2,17 +2,14 @@ import Blogs from "../Pages/Blogs/Blogs";
 import Banner from "../Banner/Banner";
 import HeroSection from "../HeroSection/HeroSection";
 import Process from "../Process/Process";
-import React from 'react'
+import React from "react";
 const Home = () => {
   return (
     <div>
-    
-      <h1>This is home</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officiis atque nostrum id sed, enim laboriosam illo odio ea ipsam fugit esse accusamus accusantium quod culpa quo unde alias! Error, eligendi!</p>
-      <Blogs></Blogs>
       <Banner />
       <HeroSection />
       <Process />
+      <Blogs></Blogs>
     </div>
   );
 };
--- src/Component/Process/Process.tsx
+++ src/Component/Process/Process.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { FaDesktop, FaBookReader, FaFacebookMessenger } from "react-icons/fa";
+
 const Process = () => {
   return (
     <div className="my-10">
@@ -16,14 +18,6 @@ const Process = () => {
             <FaBookReader />
           </div>{" "}
           <h3 className="text-2xl mt-3">2. Study, Practice and Review</h3>
-        </div>
-        <div>
-          <div className="border-2 border-black  w-36 h-36  p-9 rounded-full  text-center text-7xl">
-            <FaFacebookMessenger />
-          </div>
-          <h3 className="text-2xl mt-3">
-            3. Speak Confidently & Ace the IELTS test
-          </h3>
         </div>
         <div>
           <div className="border-2 border-black  w-36 h-36  p-9 rounded-full  text-center text-7xl">
