f810457720476a1c824ad9b2e29185f2885c6646
Azharul Islam <azharulip@gmail.com>
2023-08-22 10:02:39 +0600

    about,terms,privacy page almost done

--- src/Shared/Footer.tsx
+++ src/Shared/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -20,15 +21,15 @@ const Footer = () => {
           </div>
           <div>
             <span className="footer-title">Company</span>
-            <a className="link link-hover">About us</a>
+            <Link to={"about-us"}>About us</Link>
             <a className="link link-hover">Contact</a>
             <a className="link link-hover">Jobs</a>
             <a className="link link-hover">Press kit</a>
           </div>
           <div>
             <span className="footer-title">Legal</span>
-            <a className="link link-hover">Terms of use</a>
-            <a className="link link-hover">Privacy policy</a>
+            <Link to={"terms-and-Conditions"}>Terms of use</Link>
+           <Link to={"privacy-policy"}>Privacy policy</Link>
             <a className="link link-hover">Cookie policy</a>
           </div>
         </footer>
