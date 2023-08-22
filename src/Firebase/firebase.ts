73a3919ea377243e4ab1918b515cbcb1288438a9
Azharul Islam <azharulip@gmail.com>
2023-08-22 10:38:25 +0600

    some error fixed

--- src/Firebase/firebase.ts
+++ src/Firebase/firebase.ts
@@ -1,34 +1,16 @@
-// import { FirebaseApp, initializeApp } from "firebase/app";
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAr_oyQWe9RHvfl2qBpwVHZOvDZB7bZTYA",
-//   authDomain: "spoken-english-65d22.firebaseapp.com",
-//   projectId: "spoken-english-65d22",
-//   storageBucket: "spoken-english-65d22.appspot.com",
-//   messagingSenderId: "997742080295",
-//   appId: "1:997742080295:web:8e6785b1381ba32aa63902",
-// };
-
-// // Initialize Firebase
-// const app: FirebaseApp = initializeApp(firebaseConfig);
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { FirebaseApp, initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxVSCe2TcV8Kuftxf4HU3qxNw2NfqV8yA",
-  authDomain: "spoken-english-80e86.firebaseapp.com",
-  projectId: "spoken-english-80e86",
-  storageBucket: "spoken-english-80e86.appspot.com",
-  messagingSenderId: "68195361326",
-  appId: "1:68195361326:web:d133477269cdc301fe75bf"
+  apiKey: "AIzaSyAr_oyQWe9RHvfl2qBpwVHZOvDZB7bZTYA",
+  authDomain: "spoken-english-65d22.firebaseapp.com",
+  projectId: "spoken-english-65d22",
+  storageBucket: "spoken-english-65d22.appspot.com",
+  messagingSenderId: "997742080295",
+  appId: "1:997742080295:web:8e6785b1381ba32aa63902",
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app: FirebaseApp = initializeApp(firebaseConfig);
 
 export default app;
