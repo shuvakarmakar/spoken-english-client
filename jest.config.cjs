9a3285301b16387f5412c148e5ea992ce2a3b912
shuvakarmakar <shuva15-7953@diu.edu.bd>
2023-09-11 16:50:09 +0600

    Update Unit Test System

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    testMatch: ['**/__tests__/**/*.js'],
-    collectCoverage: true,
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-    },
-    "moduleFileExtensions": ["js", "jsx", "json"]
-
-}
