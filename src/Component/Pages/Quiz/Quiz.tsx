68ad7a4c614c089fb2419f3da9951960d5295de0
Azharul Islam <azharulip@gmail.com>
2023-09-15 17:23:01 +0600

    quiz section modification

--- src/Component/Pages/Quiz/Quiz.tsx
+++ src/Component/Pages/Quiz/Quiz.tsx
@@ -25,10 +25,13 @@ function Quiz() {
     <div className="h-[calc(100vh-50px)] flex items-center justify-center ">
       <div className=" p-6 rounded-lg shadow-2xl w-[90%] md:w-[50%] border changebg">
         {currentQuestionIndex < quizData.length ? (
+          <div className="">
+            <p className="text-center pb-3 font-bold text-2xl">{currentQuestionIndex+1} of {totalQuestions}</p>
           <QuizQuestion
             question={quizData[currentQuestionIndex]}
             onAnswer={handleAnswer}
           />
+          </div>
         ) : (
           <QuizResult
             course={course}
